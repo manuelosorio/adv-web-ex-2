99d8c85f0fa6d7d742dda2522b85579475962320
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-09-24 23:03:39 -0400

    Update 2023-09-25T03:03:38.323Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
-!function e(o,n,r){function t(c,d){if(!n[c]){if(!o[c]){var i="function"==typeof require&&require;if(!d&&i)return i(c,!0);if(u)return u(c,!0);var a=new Error("Cannot find module '"+c+"'");throw a.code="MODULE_NOT_FOUND",a}var l=n[c]={exports:{}};o[c][0].call(l.exports,(function(e){return t(o[c][1][e]||e)}),l,l.exports,e,o,n,r)}return n[c].exports}for(var u="function"==typeof require&&require,c=0;c<r.length;c++)t(r[c]);return t}({1:[function(e,o,n){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.Modal=void 0;const r=document.querySelector("#modal"),t=document.querySelector("#openModal"),u=document.querySelector("#closeModal");n.Modal=()=>{t.addEventListener("click",(()=>{r.open=!0})),u.addEventListener("click",(()=>{r.open=!1}))}},{}],2:[function(e,o,n){"use strict";var r=e("./components/modal");document.addEventListener("DOMContentLoaded",(()=>{(0,r.Modal)()}))},{"./components/modal":1}]},{},[2]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Modal = void 0;
+const modal = document.querySelector('#modal');
+const openBtn = document.querySelector('#openModal');
+const closeBtn = document.querySelector('#closeModal');
+const Modal = () => {
+  openBtn.addEventListener('click', () => {
+    modal.showModal();
+  });
+  closeBtn.addEventListener('click', () => {
+    modal.close();
+  });
+};
+exports.Modal = Modal;
+
+},{}],2:[function(require,module,exports){
+// import "core-js";
+"use strict";
+
+},{}],3:[function(require,module,exports){
+"use strict";
+
+var _modal = require("./components/modal");
+document.addEventListener('DOMContentLoaded', () => {
+  (0, _modal.Modal)();
+});
+
+},{"./components/modal":1}]},{},[1,2,3]);
